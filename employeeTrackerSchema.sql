0fdef533ec60ce65851b4c3f36b4ab175dbb3f74
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-11-04 23:19:40 -0700

    added on delete cascade to table

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -19,7 +19,7 @@ CREATE TABLE role(
   SALARY DECIMAL,
   department_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department (id)
+  FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE 
 );
 
 CREATE TABLE employee (
@@ -29,8 +29,8 @@ CREATE TABLE employee (
   role_id INT NOT NULL,
   manager_id INT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES  role (id)
-  FOREIGN KEY (manager_id) REFERENCES  employee (id)
+  FOREIGN KEY (role_id) REFERENCES  role (id) ON DELETE CASCADE
+  FOREIGN KEY (manager_id) REFERENCES  employee (id) ON DELETE CASCADE
 );
 
 
